3382e79146e8af0cc2c3987af08f91caa6a70951
Malavika007 <76998900+Malavika007@users.noreply.github.com>
2021-10-30 10:56:41 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,26 @@
+
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+
+export default class Feed extends React.Component() {
+    render(){
+
+        return (
+            <View style={styles.container}>
+              <Text>Feed</Text>
+             
+            </View>
+          );
+    }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
